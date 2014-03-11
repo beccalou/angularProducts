cf76cfc255252048967dad6d22c29c0f51304f2c
Becky Bell <rlbell@alum.bu.edu>
2014-03-11 14:47:53 -0400

    added require trees to main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,6 @@
 // TODO: Add angular application module and routes.
+//= require_self
+//= require_tree ./services/main
+//= require_tree ./filters/main
+//= require_tree ./controllers/main
+//= require_tree ./directives/main
